5ffa4c67974b910d1ed8885aaa6b11281535623b
G <G>
2020-11-11 18:49:23 +0100

    hecho

---
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/model/PersonalControl.java
@@ -0,0 +1,5 @@
+package org.springframework.samples.aerolineasAAAFC.model;
+
+public class PersonalControl {
+
+}
---
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/model/Rol.java
@@ -0,0 +1,5 @@
+package org.springframework.samples.aerolineasAAAFC.model;
+
+public enum Rol {
+
+}
