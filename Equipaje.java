21e108a717ca1beec0fe34b6b4c582de9ea775c1
carcardia <73120945+carcardia@users.noreply.github.com>
2020-11-14 11:58:29 +0100

    Add files via upload

---
+++ Equipaje.java
@@ -0,0 +1,33 @@
+package org.springframework.samples.aerolineasAAAFC.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Entidad simple que representa un equipaje
+ */
+
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "equipaje")
+  
+public class Equipaje extends IdEntity{
+	
+	//Atributos
+	
+	@Column(name = "peso")
+	@NotEmpty
+	protected Integer peso;
+	
+	@Column(name = "dimensiones")
+	@NotEmpty
+	protected String dimensiones;	
+	
+}
---
+++ PersonalOficina.java
@@ -0,0 +1,46 @@
+package org.springframework.samples.aerolineasAAAFC.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+* Entidad simple que representa un personal de oficina
+*/
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "personalOficina")
+public class PersonalOficina extends IdEntity{
+
+	//Atributos
+	
+	@Column(name = "nombre")
+	@NotEmpty
+	protected String nombre;
+	
+	@Column(name = "apellidos")
+	@NotEmpty
+	protected String apellidos;
+	
+	@Column(name = "nif")
+	@NotEmpty
+	@Pattern(regexp="^\\d{8}[a-zA-Z]$")
+	protected String nif;
+	
+	@Column(name = "iban")
+	@NotEmpty
+	@Pattern(regexp="^ES\\d{22}$")
+	protected String iban;
+	
+	@Column(name = "salario")
+	@NotEmpty
+	protected Double Salario;
+
+	
+}
