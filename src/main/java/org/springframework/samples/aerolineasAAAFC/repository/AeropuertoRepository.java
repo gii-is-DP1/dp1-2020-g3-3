23797a28e5792d769653e2dd58ef3815f985a6a9
antoniojss1 <antoniojss1a@gmail.com>
2020-11-23 19:42:49 +0100

    resolucion de un fallo

--- src/main/java/org/springframework/samples/aerolineasAAAFC/repository/AeropuertoRepository.java
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/repository/AeropuertoRepository.java
@@ -12,26 +12,10 @@ public interface AeropuertoRepository extends Repository<Aeropuerto,Integer>{
 	@Query("SELECT DISTINCT aeropuerto FROM Aeropuerto aeropuerto WHERE aeropuerto.id = :id")
 	public Aeropuerto findById(@Param("id") int id) throws DataAccessException;
 	
-	@Query("DELETE aeropuerto FROM Aeropuerto aeropuerto WHERE aeropuerto.id = :id")
+	//@Query("DELETE aeropuerto FROM Aeropuerto aeropuerto WHERE aeropuerto.id = :id")
 	public Aeropuerto deleteById(@Param("id") int id) throws DataAccessException;
 
-/*
-	@Query("UPDATE Vuelos vuelo SET vuelo.fecha_vuelo = fecha_vuelo WHERE vuelo.id =:id")
-	public Vuelo modificarFecha(@Param("fecha_vuelo") Date fechaVuelo, @Param("id") int id);
-	
-	
-	@Query("UPDATE Vuelos vuelo SET vuelo.hora_salida = hora_salida WHERE vuelo.id =:id")
-	public Vuelo modificarHoraSalida(@Param("hora_salida") Date horaSalida, @Param("id") int id);
-	
-	
-	@Query("UPDATE Vuelos vuelo SET vuelo.hora_llegada = hora_llegada WHERE vuelo.id =:id")
-	public Vuelo modificarHoraLlegada(@Param("hora_llegada") Date horaLlegada, @Param("id") int id);
-	
-	
-	@Query("UPDATE Vuelos vuelo SET vuelo.precio = precio WHERE vuelo.id =:id")
-	public Vuelo modificarPrecio(@Param("precio") Double precio, @Param("id") int id);
-	
-	*/
+
 }
 
 
