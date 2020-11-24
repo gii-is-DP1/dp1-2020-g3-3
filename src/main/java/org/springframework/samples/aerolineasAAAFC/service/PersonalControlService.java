6cd732c1df4c2776b66def00f460f7d9c4f2445d
Carolina <carcardia2@alum.us.es>
2020-11-24 20:45:34 +0100

    cambios en repository personal control

--- src/main/java/org/springframework/samples/aerolineasAAAFC/service/PersonalControlService.java
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/service/PersonalControlService.java
@@ -3,14 +3,25 @@ package org.springframework.samples.aerolineasAAAFC.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.aerolineasAAAFC.model.PersonalControl;
+import org.springframework.samples.aerolineasAAAFC.model.PersonalOficina;
 import org.springframework.samples.aerolineasAAAFC.repository.PersonalControlRepository;
+import org.springframework.samples.aerolineasAAAFC.repository.PersonalOficinaRepository;
+import org.springframework.samples.petclinic.service.AuthoritiesService;
+import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.transaction.annotation.Transactional;
 
 public class PersonalControlService {
 
+
 	private PersonalControlRepository pControlRepository;
 	
-	@Autowired 
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AuthoritiesService authoritiesService;
+	
+	@Autowired
 	public PersonalControlService(PersonalControlRepository pControlRepository) {
 		this.pControlRepository = pControlRepository;
 	}
@@ -18,10 +29,13 @@ public class PersonalControlService {
 	@Transactional
 	public void savePersonalControl(PersonalControl pControl) throws DataAccessException{
 		pControlRepository.save(pControl);
+		userService.saveUser(pControl.getUser());
+		authoritiesService.saveAuthorities(pControl.getUser().getUsername(), "personal");
 	}
 	
 	@Transactional(readOnly=true)
 	public PersonalControl findPersonalControlById(int id) throws DataAccessException{
 		return pControlRepository.findById(id);
 	}
+	
 }
