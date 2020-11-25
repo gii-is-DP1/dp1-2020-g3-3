77f0a256c02777252c8c9fa7e44dc3e1f89260bd
antoniojss1 <antoniojss1a@gmail.com>
2020-11-25 20:17:23 +0100

    introduccion de los datos en data y otras cosas

--- src/test/java/org/springframework/samples/aerolineasAAAFC/service/PersonalOficinaServiceTests.java
+++ src/test/java/org/springframework/samples/aerolineasAAAFC/service/PersonalOficinaServiceTests.java
@@ -2,7 +2,7 @@ package org.springframework.samples.aerolineasAAAFC.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
