67fbf0d2fd74e57e1bced74ec6751333185ac6f2
antoniojss1 <antoniojss1a@gmail.com>
2020-11-25 20:31:32 +0100

    He hecho cosas en data
    "Ahora con ganas"

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,17 +1,17 @@
 INSERT INTO aeropuertos
-VALUES ("Aeropuerto de São Paulo Guarulhos","São Paulo, Brasil" , "GRU", "+55 11 2445 2945")
+VALUES ('Aeropuerto de São Paulo Guarulhos','São Paulo, Brasil' , 'GRU', '+55 11 2445 2945')
 
 INSERT INTO aviones
-VALUES ("Airbus A320", 300, 2, 1700, "2015/09/24", true, "2017/09/24", 200, 80, 20)
+VALUES ('Airbus A320', 300, 2, 1700, '2015/09/24', true, '2017/09/24', 200, 80, 20)
 
 INSERT INTO azafatos
-VALUES ("Ana", "Silfes Guerrero", "21333214R", "ES 8721000893201234567892", "Inglés, Francés, Español", 2500)
+VALUES ('Ana', 'Silfes Guerrero', '21333214R', 'ES 8721000893201234567892', 'Inglés, Francés, Español', 2500)
 
 INSERT INTO billetes
-VALUES (80,"F4","2020/04/06",ECONOMICA)
+VALUES (80,'F4','2020/04/06',ECONOMICA)
 
 INSERT INTO clientes
-VALUES ("Juan Jesús", "Ferrero Gutiérrez", "28976897W", "Calle Carbón", "35 - 41007 Sevilla", "ES 6621000418401234567893", 03/06/1997)
+VALUES ('Juan Jesús', 'Ferrero Gutiérrez', '28976897W', 'Calle Carbón', '35 - 41007 Sevilla', 'ES 6621000418401234567893', 03/06/1997)
 
 
 INSERT INTO vuelos(fecha_vuelo,hora_salida,hora_llegada,coste) 
