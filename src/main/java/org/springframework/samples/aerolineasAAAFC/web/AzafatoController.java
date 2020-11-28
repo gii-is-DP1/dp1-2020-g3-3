e89206946c0d03fd2d1b7e1d46fa434e28f21160
Carolina <carcardia2@alum.us.es>
2020-11-28 20:28:25 +0100

    Controladores de Azafato y Personal de Control

---
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/web/AzafatoController.java
@@ -0,0 +1,101 @@
+package org.springframework.samples.aerolineasAAAFC.web;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.aerolineasAAAFC.model.Azafato;
+import org.springframework.samples.aerolineasAAAFC.model.PersonalOficina;
+import org.springframework.samples.aerolineasAAAFC.service.AzafatoService;
+import org.springframework.samples.aerolineasAAAFC.service.PersonalOficinaService;
+import org.springframework.samples.aerolineasAAAFC.service.exceptions.IbanDuplicadoException;
+import org.springframework.samples.aerolineasAAAFC.service.exceptions.NifDuplicadoException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AzafatoController {
+
+	private static final String VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM = "azafatos/createOrUpdateAzafatoForm";
+	
+	private final AzafatoService azafatoService;
+	
+	@Autowired
+	public AzafatoController(AzafatoService azafatoService) {
+		this.azafatoService = azafatoService;
+	}
+	
+	@InitBinder
+	public void setAllowedFields(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
+	}
+	
+	/*
+	 * Alta de un nuevo azafato
+	 */
+	@GetMapping(value = "/azafatos/new")
+	public String initCreationAzafatoForm(Map<String, Object> model) {
+		Azafato azafato = new Azafato();
+		model.put("azafato", azafato);
+		return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/azafatos/new")
+	public String processCreationPersonalOficinaForm(@Valid Azafato azafato, BindingResult result) {
+		if(result.hasErrors()) {
+			return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			try {
+				this.azafatoService.saveAzafato(azafato);
+			} catch (NifDuplicadoException e) {
+				result.rejectValue("nif", "duplicate", "already exists");
+				e.printStackTrace();
+			} catch (IbanDuplicadoException e) {
+				result.rejectValue("iban", "duplicate", "already exists");
+				return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+			}
+			
+			return "redirect:/azafatos/" + azafato.getId();
+		}
+	}
+	
+	/*
+	 *  Update sobre un azafato
+	 */
+	@GetMapping(value = "/azafatos/{azafatoId}/edit")
+	public String initUpdateAzafatoForm(@PathVariable("azafatoId") int azafatoId, Model model) {
+		Azafato azafato = this.azafatoService.findAzafatoById(azafatoId);
+		model.addAttribute(azafato);
+		return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/azafatos/{azafatoId}/edit")
+	public String processUpdateAzafatoForm(@Valid Azafato azafato, BindingResult result, @PathVariable("azafatoId") int azafatoId) {
+		if(result.hasErrors()) {
+			return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			azafato.setId(azafatoId);
+			try {
+				this.azafatoService.saveAzafato(azafato);
+			} catch (NifDuplicadoException e) {
+				result.rejectValue("nif", "duplicate", "already exists");
+				return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+			} catch (IbanDuplicadoException e) {
+				result.rejectValue("iban", "duplicate", "already exists");
+				return VIEWS_AZAFATO_CREATE_OR_UPDATE_FORM;
+			}
+			
+			return "redirect:/azafatos/{azafatoId}";
+		}
+	}
+}
---
+++ src/main/java/org/springframework/samples/aerolineasAAAFC/web/PersonalControlController.java
@@ -0,0 +1,99 @@
+package org.springframework.samples.aerolineasAAAFC.web;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.aerolineasAAAFC.model.PersonalControl;
+import org.springframework.samples.aerolineasAAAFC.service.PersonalControlService;
+import org.springframework.samples.aerolineasAAAFC.service.exceptions.IbanDuplicadoException;
+import org.springframework.samples.aerolineasAAAFC.service.exceptions.NifDuplicadoException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PersonalControlController {
+
+	private static final String VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM = "controladores/createOrUpdatePersonalControlForm";
+	
+	private final PersonalControlService pControlService;
+	
+	@Autowired
+	public PersonalControlController(PersonalControlService pControlService) {
+		this.pControlService = pControlService;
+	}
+	
+	@InitBinder
+	public void setAllowedFields(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
+	}
+	
+	/*
+	 * Alta de un nuevo controlador
+	 */
+	@GetMapping(value = "/controladores/new")
+	public String initCreationPersonalControlForm(Map<String, Object> model) {
+		PersonalControl pControl = new PersonalControl();
+		model.put("personalControl", pControl);
+		return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/controladores/new")
+	public String processCreationPersonalControlForm(@Valid PersonalControl pControl, BindingResult result) {
+		if(result.hasErrors()) {
+			return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			try {
+				this.pControlService.savePersonalControl(pControl);
+			} catch (NifDuplicadoException e) {
+				result.rejectValue("nif", "duplicate", "already exists");
+				e.printStackTrace();
+			} catch (IbanDuplicadoException e) {
+				result.rejectValue("iban", "duplicate", "already exists");
+				return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+			}
+			
+			return "redirect:/oficinistas/" + pControl.getId();
+		}
+	}
+	
+	/*
+	 *  Update sobre un controlador
+	 */
+	@GetMapping(value = "/controladores/{pControlId}/edit")
+	public String initUpdatePersonalControlForm(@PathVariable("pControlId") int pControlId, Model model) {
+		PersonalControl pControl = this.pControlService.findPersonalControlById(pControlId);
+		model.addAttribute(pControl);
+		return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+	}
+	
+	@PostMapping(value = "/controladores/{pControlId}/edit")
+	public String processUpdatePersonalControlForm(@Valid PersonalControl pControl, BindingResult result, @PathVariable("pControlId") int pControlId) {
+		if(result.hasErrors()) {
+			return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+		}
+		else {
+			pControl.setId(pControlId);
+			try {
+				this.pControlService.savePersonalControl(pControl);
+			} catch (NifDuplicadoException e) {
+				result.rejectValue("nif", "duplicate", "already exists");
+				return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+			} catch (IbanDuplicadoException e) {
+				result.rejectValue("iban", "duplicate", "already exists");
+				return VIEWS_PERSONALCONTROL_CREATE_OR_UPDATE_FORM;
+			}
+			
+			return "redirect:/controladores/{pControlId}";
+		}
+	}
+}
