8289505ed71311f9ad0b2e215d0525b657d9e31c
felipescalera <felescgon@alum.us.es>
2021-01-04 23:36:59 +0100

    Revert "Revert "Fixeando cositas en los controladores de azafatos (ya va)""
    This reverts commit d08735fdd7bc8f8588d7ab87ce9c6074e1626259.

--- src/test/java/org/springframework/samples/aerolineasAAAFC/service/AzafatoServiceTests.java
+++ src/test/java/org/springframework/samples/aerolineasAAAFC/service/AzafatoServiceTests.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,10 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.samples.aerolineasAAAFC.model.Azafato;
 import org.springframework.samples.aerolineasAAAFC.model.IdiomaType;
 import org.springframework.samples.aerolineasAAAFC.model.User;
 import org.springframework.samples.aerolineasAAAFC.model.Vuelo;
+import org.springframework.samples.aerolineasAAAFC.service.exceptions.IbanDuplicadoException;
 import org.springframework.samples.aerolineasAAAFC.service.exceptions.IdiomasNoSuficientesException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -79,8 +84,8 @@ public class AzafatoServiceTests {
 
 		azafato.setNombre("Rafael");
 		azafato.setApellidos("Nadal Parera");
-		azafato.setIban("ES 33335905228408934564567");
-		azafato.setNif("23165805A");
+		azafato.setIban("ES 1201284722814468736382");
+		azafato.setNif("84493294B");
 		azafato.setSalario(2340.);
 		
 		IdiomaType i1 = new IdiomaType();
@@ -91,7 +96,7 @@ public class AzafatoServiceTests {
 		azafato.setIdiomas(idiomas);
 		
 		User user = new User();
-		user.setUsername("23165805A");
+		user.setUsername("84493294B");
 		user.setPassword("*pepe_csfay");
 		user.setEnabled(true);
 		azafato.setUser(user);    
@@ -102,6 +107,60 @@ public class AzafatoServiceTests {
 
 		azafatos = this.azafatoService.findAzafatos();
 		assertThat(azafatos.size()).isEqualTo(found);
+
+	}
+	
+	@Test
+	@Transactional
+	public void shouldInsertAzafato2Idiomas() {
+
+		Collection<Azafato> azafatos = this.azafatoService.findAzafatos();
+		int found = azafatos.size();
+		
+		Azafato azafato = new Azafato();
+
+		azafato.setNombre("Rafael");
+		azafato.setApellidos("Nadal Parera");
+		azafato.setIban("ES 3332020458401202067812");
+		azafato.setNif("84493294B");
+		azafato.setSalario(2340.);
+		
+		IdiomaType i1 = new IdiomaType();
+		i1.setIdioma("ES");
+		IdiomaType i2 = new IdiomaType();
+		i2.setIdioma("FR");
+		Set<IdiomaType> idiomas = new HashSet<IdiomaType>();
+		idiomas.add(i1);
+		idiomas.add(i2);
+		
+		azafato.setIdiomas(idiomas);
+		
+		User user = new User();
+		user.setUsername("84493294B");
+		user.setPassword("*pepe_csfay");
+		user.setEnabled(true);
+		azafato.setUser(user);    
+
+		try {
+			azafatoService.saveAzafato(azafato);
+		} catch (DataIntegrityViolationException e) {
+            Logger.getLogger(AzafatoServiceTests.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		} catch (DataAccessException e) {
+            Logger.getLogger(AzafatoServiceTests.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		} catch (IbanDuplicadoException e) {
+            Logger.getLogger(AzafatoServiceTests.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		} catch (IdiomasNoSuficientesException e) {
+            Logger.getLogger(AzafatoServiceTests.class.getName()).log(Level.SEVERE, null, e);
+			e.printStackTrace();
+		}
+
+		azafatos = this.azafatoService.findAzafatos();
+		assertThat(azafatos.size()).isEqualTo(found + 1);
+		
+		assertThat(azafato.getId()).isNotNull();
 
 	}
 	
